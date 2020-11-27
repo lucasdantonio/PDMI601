c1788695d9761ef126d0d7142799d2ab05139b79
Lucas D'Antonio Navarro de Andrade <lucasdantonio@outlook.com>
2020-11-27 18:06:48 -0300

    second upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PDM_TP01
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            //btn1.Clicked += onClick;
+        }
+
+        void onClickOk(object sender, EventArgs args)
+        {
+            print("[INFO] função onClickOk\n");
+            if (user.Text.Contains("@") && user.Text.Contains(".com"))
+            {
+                print("[INFO]" + user.Text  + " logado com sucesso!");
+            }
+            else {
+                print("[ERRO] identificador inválido\n");
+            }
+        }
+        void onClickLimpar(object sender, EventArgs args)
+        {
+            print("[INFO] função onClickLimpar\n");
+            user.Text = "";
+            pass.Text = "";
+            consoleLog.Text = "";
+        }
+        void onClickCreditos(object sender, EventArgs args)
+        {
+            print("[INFO] onClickCreditos\n");
+            print("Criado por: Lucas D'Antônio CB-300161X e Gabriel Brito - CB3002373\n");
+        }
+
+        void print(String message) {
+            consoleLog.Text += message;
+        }
+
+    }
+}
